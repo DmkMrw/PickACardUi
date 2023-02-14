fbcc99e335d3b5e668d2aef3e06f1c62fa9e9190
DmkMrw <dmrowka7@gmail.com>
2023-02-14 09:08:06 +0100

    Finish app

--- PickACardUi/MainWindow.xaml.cs
+++ PickACardUi/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace PickACardUi
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] pickedCards = CardPicker.PickSomeCards((int)numberOfCards.Value);
+            listOfCards.Items.Clear();
+            foreach (string card in pickedCards)
+            {
+                listOfCards.Items.Add(card);
+            }
+        }
     }
 }
